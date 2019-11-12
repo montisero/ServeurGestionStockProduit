3ef0c807d292067c5950f51a10e6d5cda3606380
Montassar <montassar.amri@gmail.com>
2019-11-12 16:05:13 +0100

    first commit

---
+++ src/main/java/com/GestionStockProduit/ServeurGestionStockProduit/ServeurGestionStockProduitApplication.java
@@ -0,0 +1,14 @@
+package com.GestionStockProduit.ServeurGestionStockProduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProduitApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ServeurGestionStockProduitApplication.class, args);
+	}
+
+}
